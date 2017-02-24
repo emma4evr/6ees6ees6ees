b95ce94ad42f038a9df8a03058b5cd577439fb68
Emma Rios <tigersoundtracks@gmail.com>
2017-02-24 13:13:46 -0700

    Completed 6ees
    All tests pass.

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,14 @@
 class Grub {
-};
+  constructor () {
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+  }
+  eat () {
+    return 'Mmmmmmmmm jelly';
+  }
+}
+
+/*
+This assignment must be written with ES6 classes by using the new class, extends, and super keywords
+*/
